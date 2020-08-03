277f16e6a4f4590f876e156a2c50a3673e1b4380
조윤영 <yoon980208@gmail.com>
2020-08-04 02:00:53 +0900

    [UPDATE] 라인차트 + 파이차트 커스텀

--- Charts/Charts/CustomChartView.swift
+++ Charts/Charts/CustomChartView.swift
@@ -18,11 +18,13 @@ class PieChartView: UIView, CAAnimationDelegate {
     var colors: [UIColor] = []
     var currentIndex: Int = 0
     var myCenter = CGPoint.zero
+    var graphiWdth: CGFloat = 0
   
-    init(frame: CGRect, values: [CGFloat], colors:[UIColor]) {
+    init(frame: CGRect, values: [CGFloat], colors:[UIColor], width:CGFloat) {
         super.init(frame: frame)
         self.values = values
         self.colors = colors
+        self.graphiWdth = width
     }
     
     required init?(coder: NSCoder) {
@@ -38,7 +40,7 @@ class PieChartView: UIView, CAAnimationDelegate {
         let animation = CABasicAnimation(keyPath: "strokeEnd")
         animation.fromValue = 0
         animation.toValue = 1
-        animation.duration = 1
+        animation.duration = 0.5
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
         animation.delegate = self
         let total = values.reduce(0, +)
@@ -48,11 +50,13 @@ class PieChartView: UIView, CAAnimationDelegate {
         let sliceLayer = CAShapeLayer()
         sliceLayer.path = path.cgPath
         sliceLayer.fillColor = nil
-        sliceLayer.strokeColor = self.colors.randomElement()?.cgColor
-        sliceLayer.lineWidth = 80
+        sliceLayer.strokeColor = self.colors[currentIndex].cgColor
+
+        sliceLayer.lineWidth = CGFloat(graphiWdth)
+        
         sliceLayer.strokeEnd = 1
         sliceLayer.add(animation, forKey: animation.keyPath)
-    
+        
         self.layer.addSublayer(sliceLayer)
     }
   
@@ -74,6 +78,7 @@ class LineGraphView: UIView {
     var animated: Bool!
   
     let graphLayer = CAShapeLayer()
+    let xLayer = CAShapeLayer()
   
     init(frame: CGRect, values: [CGFloat], animated: Bool) {
         super.init(frame: frame)
@@ -87,6 +92,41 @@ class LineGraphView: UIView {
   
     override func draw(_ rect: CGRect) {
         super.draw(rect)
+        
+        
+        //MARK: x좌표선 선 그리기
+        let xPath = UIBezierPath()
+        xPath.lineWidth = 0.5
+        xPath.move(to: CGPoint(x: 0, y: self.frame.height))
+        xPath.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        
+        xLayer.path = xPath.cgPath
+        
+        xLayer.strokeColor = #colorLiteral(red: 0.3921568627, green: 0.3921568627, blue: 0.3921568627, alpha: 1)
+        self.layer.addSublayer(xLayer)
+        
+        //MARK: x 그리드 선 그리기
+        var xGrideHeight:CGFloat = 0
+        
+        for i in 0..<4 {
+            let xGridLayer = CAShapeLayer()
+            let xGridPath = UIBezierPath()
+            
+            xGridLayer.lineWidth = 1
+            xGridLayer.lineDashPattern = .some([2,2])
+
+            xGrideHeight += (self.frame.height / 5)
+
+            xGridPath.move(to: CGPoint(x: 0, y: xGrideHeight))
+            xGridPath.addLine(to: CGPoint(x: self.frame.width, y: xGrideHeight))
+            
+            xGridLayer.path = xGridPath.cgPath
+            
+            xGridLayer.strokeColor = #colorLiteral(red: 0.7529411765, green: 0.7529411765, blue: 0.7529411765, alpha: 1)
+            self.layer.addSublayer(xGridLayer)
+        }
+
+        //MARK: Line차트 그래프 그리기
         self.graphPath = UIBezierPath()
         self.zeroPath = UIBezierPath()
         
@@ -95,35 +135,105 @@ class LineGraphView: UIView {
         let xOffset: CGFloat = self.frame.width / CGFloat(values.count)
         
         var currentX: CGFloat = 0
-        let startPosition = CGPoint(x: currentX, y: self.frame.height)
+        let startPosition = CGPoint(x:currentX, y: self.frame.height)
         self.graphPath.move(to: startPosition)
         self.zeroPath.move(to: startPosition)
         
         for i in 0..<values.count {
             currentX += xOffset
-            let newPosition = CGPoint(x: currentX,
-                                    y: self.frame.height - self.values[i])
+            let newPosition = CGPoint(x: currentX, y: self.frame.height - self.values[i])
             self.graphPath.addLine(to: newPosition)
             self.zeroPath.addLine(to: CGPoint(x: currentX, y: self.frame.height))
         }
         
-        graphLayer.fillColor = nil
-        graphLayer.strokeColor = UIColor.black.cgColor
-        graphLayer.lineWidth = 2
+        self.graphPath.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        self.zeroPath.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        
+        graphLayer.fillColor = #colorLiteral(red: 0.5411764706, green: 0.7960784314, blue: 0.9647058824, alpha: 0.36)
+        graphLayer.strokeColor = nil
         
         let oldPath = self.zeroPath.cgPath
         let newPath = self.graphPath.cgPath
-        
+
         if self.animated {
             let animation = CABasicAnimation(keyPath: "path")
-            animation.duration = 1.0
+            animation.duration = 0.5
             animation.fromValue = oldPath
             animation.toValue = newPath
         
           self.graphLayer.path = newPath
           self.graphLayer.add(animation, forKey: "path")
         }
-      
+
         self.graphLayer.path = newPath
+    
+    }
+}
+
+class BarGraphView: UIView {
+    var values: [CGFloat] = []
+    var colors: [UIColor] = []
+    var animated: Bool!
+  
+    var graphPath: UIBezierPath!
+    var zeroPath: UIBezierPath!
+  
+    let graphLayer = CAShapeLayer()
+  
+    init(frame: CGRect, values: [CGFloat], colors: [UIColor], animated: Bool) {
+        super.init(frame: frame)
+        self.values = values
+        self.colors = colors
+        self.animated = animated
+      }
+  
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+  
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        //Bar차트 그래프 그리기
+        
+        var currentX: CGFloat = 0
+        
+        for i in 0..<values.count {
+            self.graphPath = UIBezierPath()
+            self.zeroPath = UIBezierPath()
+            
+            self.layer.addSublayer(graphLayer)
+            graphLayer.lineWidth = 20
+            
+            let xOffset: CGFloat = self.frame.width / CGFloat(values.count)
+            
+            let startPosition = CGPoint(x:xOffset, y: self.frame.height)
+            self.graphPath.move(to: startPosition)
+            self.zeroPath.move(to: startPosition)
+            
+            currentX += xOffset
+            let newPosition = CGPoint(x: currentX, y: self.frame.height - self.values[i])
+            self.graphPath.addLine(to: newPosition)
+            self.zeroPath.addLine(to: CGPoint(x: currentX, y: self.frame.height))
+            
+            graphLayer.fillColor = #colorLiteral(red: 0.5411764706, green: 0.7960784314, blue: 0.9647058824, alpha: 0.36)
+            graphLayer.strokeColor = self.colors[i].cgColor
+            
+            let oldPath = self.zeroPath.cgPath
+            let newPath = self.graphPath.cgPath
+
+            if self.animated {
+                let animation = CABasicAnimation(keyPath: "path")
+                animation.duration = 0.5
+                animation.fromValue = oldPath
+                animation.toValue = newPath
+            
+              self.graphLayer.path = newPath
+              self.graphLayer.add(animation, forKey: "path")
+            }
+
+            self.graphLayer.path = newPath
+            
+        }
     }
 }
--- Charts/Charts/ViewController.swift
+++ Charts/Charts/ViewController.swift
@@ -11,27 +11,41 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         addLineChart()
         addPieChart()
+        addBarChart()
     }
     
     func addLineChart() {
         let lineChartFrame = CGRect(x: 0, y: 0, width: self.view1.frame.width, height: self.view1.frame.height)
-        let LineChartView = LineGraphView(frame: lineChartFrame, values: [20, 10, 30, 20, 50, 100, 10, 10], animated: true)
+        
+        let LineChartView = LineGraphView(frame: lineChartFrame, values: [20, 10, 30, 20, 50, 100, 10, 10, 0], animated: true)
         LineChartView.center = CGPoint(x: self.view1.frame.size.width / 2, y: self.view1.frame.size.height / 2)
         self.view1.addSubview(LineChartView)
+        
     }
     
     func addPieChart() {
         let pieChartFrame = CGRect(x: 0, y: 0, width: self.view2.frame.width, height: self.view2.frame.height)
-        let colors = [UIColor.black, UIColor.yellow, UIColor.red, UIColor.systemPink]
-        let pieChartView = PieChartView(frame: pieChartFrame, values: [20,60,10,80], colors: colors)
+        let pieChartColors = [ #colorLiteral(red: 0.537254902, green: 0.8, blue: 0.9647058824, alpha: 1), #colorLiteral(red: 0.7960784314, green: 0.9215686275, blue: 1, alpha: 1), #colorLiteral(red: 0.9490196078, green: 0.9803921569, blue: 1, alpha: 1), #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1) ]
+        let pieChartView = PieChartView(frame: pieChartFrame, values: [53,17,15,15], colors: pieChartColors, width: 50)
         pieChartView.center = CGPoint(x:self.view2.frame.size.width / 2, y: self.view2.frame.size.height / 2)
         self.view2.addSubview(pieChartView)
+    
+    }
+    
+    func addBarChart() {
+        let barChartFrame = CGRect(x: 0, y: 0, width: self.view3.frame.width, height: self.view3.frame.height)
+        let barChartColors = [ #colorLiteral(red: 0.8, green: 0.9215686275, blue: 1, alpha: 1), #colorLiteral(red: 0.5411764706, green: 0.7960784314, blue: 0.9647058824, alpha: 1), #colorLiteral(red: 0.9490196078, green: 0.9803921569, blue: 1, alpha: 1)]
+        let barChartView = BarGraphView(frame: barChartFrame, values: [43, 66, 20], colors: barChartColors, animated: true)
+        
+        barChartView.center = CGPoint(x: self.view3.frame.size.width / 2, y: self.view3.frame.size.height / 2)
+        self.view3.addSubview(barChartView)
     }
 
 }
